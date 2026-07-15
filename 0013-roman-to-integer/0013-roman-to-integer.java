806eda4c6646a7c1836a248c4b0dae80c77c6654
Aryan tiwari <aryan0866.becse24@chitkara.edu.in>
2026-07-15 10:08:57 +0530

    Time: 2 ms (100%), Space: 46.4 MB (63.21%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int romanToInt(String s) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currValue = 0;
+            
+            switch (s.charAt(i)) {
+                case 'I': currValue = 1; break;
+                case 'V': currValue = 5; break;
+                case 'X': currValue = 10; break;
+                case 'L': currValue = 50; break;
+                case 'C': currValue = 100; break;
+                case 'D': currValue = 500; break;
+                case 'M': currValue = 1000; break;
+            }
+
+            if (currValue < prevValue) {
+                result -= currValue;
+            } else {
+                result += currValue;
+            }
+            
+            prevValue = currValue;
+        }
+
+        return result;
+    }
+}
