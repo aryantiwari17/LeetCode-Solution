08b55a0f6dbf2cb15313955ea51fa0df53b6168e
Aryan tiwari <aryan0866.becse24@chitkara.edu.in>
2026-08-22 16:44:15 +0530

    Time: 0 ms (100%), Space: 80.3 MB (40.32%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,36 +1,16 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        
-        int start = 0 ; 
-        int end  = arr.length -1;
+        int start = 0 ;
+        int end = arr.length -1;
         while(start < end){
-        int mid = start +(end-start)/2;
-            if(arr[mid]<arr[mid+1]){
+            int mid = start +(end -start)/2;
+            if(arr[mid]<arr[mid+1]){ // it means we are in asc part ok
                 start = mid+1;
             }
-            else end = mid;
+            else{ 
+                end = mid; // kyunki mid hi chota hai  pichle se toh mid ko toh aage kyu dekhana haina 
+                }
         }
-       return start; // we cam return end also because it will run till start == end 
+        return start ; // we can return end too as start and end both are same; 
     }
 }
-
-
-
-
-
-// class Solution {
-//     public int peakIndexInMountainArray(int[] arr) {
-
-//         // brute force 
-//         int max =0 ; 
-//         for(int i = 0 ; i<arr.length ;i++){
-//             if(arr[i] > arr[max]){
-//                 max = i;
-//             }
-//         }
-//         return max;
-        
-
-        
-//     }
-// }
